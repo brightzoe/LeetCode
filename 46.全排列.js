bf365c50e57dc0b9e51673ffdc7c930a12d79a9a
zoe <2577213154@qq.com>
2020-09-12 22:47:17 +0800

    2020/09/12 周六 22:47:17.59

--- 46.全排列.js
+++ 46.全排列.js
@@ -9,8 +9,28 @@
  * @param {number[]} nums
  * @return {number[][]}
  */
-var permute = function(nums) {
-  
+var permute = function (nums) {
+	const res = [];
+	const used = {};
+	dfs([])
+
+	function dfs(path) {
+		if (path.length === nums.length) {
+			//这一种情况结束了
+			res.push(path.slice());
+			return;
+		}
+		for (let num of nums) {
+			if (!used[num]) {
+				path.push(num);
+				used[num] = true;
+				dfs(path);
+				path.pop();//回到之前状态
+				used[num]=false
+			}
+		}
+	}
+		return res;
+
 };
 // @lc code=end
-
